dba3cdbe406a0ec4c303cccbf73eefaf8500d8d8
janikagahalot <janikagahalot@vikass-MacBook-Pro.local>
2015-12-09 01:47:39 +0530

    updates

--- app/src/main/java/com/appusagemonitor/MainActivity.java
+++ app/src/main/java/com/appusagemonitor/MainActivity.java
@@ -2,9 +2,11 @@ package com.appusagemonitor;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -12,7 +14,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.add(R.id.container,AppUsageMonitorFragment.newInstance()).commit();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.add(R.id.container, AppUsageMonitorFragment.newInstance()).commit();
+        }else{
+            Toast.makeText(getBaseContext(), getString(R.string.invalid_version), Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
